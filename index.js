b0cf910901cb1c0f24759497c86c108980370be2
richieok <richie.okoro@gmail.com>
2021-09-07 15:46:48 +0100

    First commit backend

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const HOST = process.env.HOST || 'http://localhost';
+const PORT = process.env.PORT || '3000';
+
+app.use(express.static('public'));
+
+app.listen(PORT, ()=>{
+    console.log(`App listening on ${HOST}:${PORT}`);
+});
